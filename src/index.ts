7534f5bb774b9e472b18c162be2c9923fcff9196
William Oliveira <william.bra@gmail.com>
2022-04-15 17:07:05 -0300

    first operational version

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,49 @@
 #!/usr/bin/env node
 import { initializeApp } from 'firebase/app'
 import { getFirestore, doc, getDoc } from 'firebase/firestore'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import fs from 'fs'
+import path from 'path'
 
-const { apiKey, appId, authDomain, databaseURL, messagingSenderId, projectId, storageBucket } = process.env
-const [ _, __, establishmentId ] = process.argv
+const argv = await yargs(hideBin(process.argv))
+  .usage('Usage: $0 <establishment_id> [options]')
+  .option('data', {
+    demandOption: true,
+    describe: 'Path to save the data file',
+    type: 'string'
+  })
+  .option('partials-dir', {
+    demandOption: true,
+    describe: 'Path to the dir to save the handlebars partials files',
+    type: 'string'
+  })
+  .demandCommand(1, 1, 'You need to provide an establishment id', 'You must provide only a establishment id and nothing else besides options.')
+  .demandOption(['data', 'partials-dir'])
+  .argv
+const establishmentId = `${argv._[0]}`
+const dataPath = `${argv.data}`
+const partialsDir = `${argv.partialsDir}`
+const { projectId, measurementId } = process.env
 
-console.log(` ------- starting box6utils with ------- `)
+console.log(`------- starting box6utils with -------`)
 console.log(`:: ENVIRONMENTAL VARIABLES ::`)
-console.log(`apiKey: `, apiKey)
-console.log(`appId: `, appId)
-console.log(`authDomain: `, authDomain)
-console.log(`databaseURL: `, databaseURL)
-console.log(`messagingSenderId: `, messagingSenderId)
 console.log(`projectId: `, projectId)
-console.log(`storageBucket: `, storageBucket)
+console.log(`measurementId: `, measurementId)
 console.log(``)
 console.log(`:: ARGUMENTS ::`)
-console.log(`$1 (establishmentId): `, establishmentId)
-console.log(` -------------------------------------- `)
+console.log(`establishmentId: `, establishmentId)
+console.log(`--data: `, dataPath)
+console.log(`--partialsDir: `, partialsDir)
+console.log(`--------------------------------------`)
+console.log(``)
 
-if ((establishmentId ?? '').length === 0) {
-  process.stdout.write(`missing arguments\n`)
+if (!projectId) {
+  console.error(`projectId must be informed as an environmental variable!`)
+} else if (!measurementId) {
+  console.error(`measurementId must be informed as an environmental variable!`)
 } else {
-  const app = initializeApp({
-    apiKey,
-    appId,
-    authDomain,
-    databaseURL,
-    messagingSenderId,
-    projectId,
-    storageBucket
-  })
+  const app = initializeApp({ projectId })
 
   const db = getFirestore(app)
 
@@ -38,17 +51,36 @@ if ((establishmentId ?? '').length === 0) {
   const snapshot = await getDoc(ref)
 
   if (!snapshot.exists()) {
-    console.error("Establishment document not found!")
+    console.error(`firestore document /establishments/${establishmentId} not found!`)
   } else {
-    const data = snapshot.data()
-    console.log('Document data:', data)
-    // const plans = Object.entries(data.plans).reduce((acc, [key, value]) => [...acc, {
-    //   ...value,
-    //   code: key,
-    //   actionText: 'ASSINE JÁ',
-    //   isHotPage: true,
-    //   isSelected: false,
-    //   isEdit: false
-    // }], []).sort((a, b) => a.order - b.order)
+    const { plans } = snapshot.data()
+    const pricing = Object.entries<any>(plans).reduce<any>((acc, [key, value]) => [...acc, {
+      ...value,
+      code: key,
+      actionText: 'ASSINE JÁ',
+      isHotPage: true,
+      isSelected: false,
+      isEdit: false
+    }], []).sort((a: any, b: any) => a.order - b.order)
+
+    const data = {
+      pricing,
+      gaTag: measurementId,
+      builtAt: Number(new Date())
+    }
+  
+    const json = JSON.stringify(data)
+    await fs.promises.mkdir(path.dirname(dataPath), { recursive: true })
+    await fs.promises.writeFile(dataPath, json, 'utf8')
+
+    await fs.promises.mkdir(partialsDir, { recursive: true })
+    await Promise.all([
+      'freight.hbs',
+      'pricing_section.hbs'
+    ].map(file => fs.promises.copyFile(`hbs/${file}`, path.join(partialsDir, file))))
+
+    console.log(`Files generated successfully!`)
+    process.exit(0)
   }
 }
+process.exit(1)
